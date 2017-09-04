0423db90f709253bebddd7c2443a9e0b82f48159
Emily Morgan <emorgan05@gmail.com>
2017-09-04 18:05:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,13 +26,29 @@ class TicTacToe
     input.to_i - 1
   end
 
-  def move(index, player_token = "X")
-    @board[index] = player_token
+  def move(player_token = "X")
+    @board[@index] = player_token
   end
 
-  def position_taken?(index)
-    !(@board[index].nil? || @board[index] == " ")
+  def position_taken?
+    !(@board[@index].nil? || @board[@index] == " ")
   end
 
-  
+  def valid_move?
+    if @index.between?(0, 8) && position_taken? == false
+      true
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    @index = input_to_index(input)
+    if valid_move?
+      move(current_player)
+      display_board
+    else
+      turn
+    end
+  end
 end
